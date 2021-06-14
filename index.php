950e43d0b446c1c75d7966956be6baba1950772b
Suriya <35035732+suriyadev@users.noreply.github.com>
2021-06-14 21:23:16 +0530

    Update index.php

--- index.php
+++ index.php
@@ -15,35 +15,15 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-<script>
-
-let base_endpoint = "https://www.youtube.com/embed/";
-
-
-function loadVideo(){
-
- let url = document.getElementById('video_id').value;
-  
-
- document.getElementById('ytubeVideo').src = base_endpoint + url.split('/')[url.split('/').length-1]+"?autoplay=1";
+ <style>
+	 body {
+    margin: 0;
 }
-
-</script>
-
+	</style>
 </head>
 <body>
-<table style="
-    margin: 112px;
-">
-<tr><td>
-<input type="text" id="video_id" /></td></tr><tr><td>
-<center><button onclick="loadVideo()" class="btn btn-success btn-lg">Load</button></center></td></tr>
-</table>
-
-
-</br>
-</hr>
-<iframe  width="500" height="500" src="<?php echo 'https://www.youtube.com/embed/' . $_GET["id"]; ?>" id="ytubeVideo">
+ 
+<iframe  width="500" height="500" src="<?php echo 'https://www.youtube.com/embed/' . $_GET["id"]; ?>" id="ytubeVideo" frameborder="0">
 </iframe>
 
 
